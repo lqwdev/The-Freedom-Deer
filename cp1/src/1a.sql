a7d01468e5b5db80189a65c20e43edec2d396581
Qingwei Lan <qingweilan2022@u.northwestern.edu>
2021-10-13 16:44:34 -0500

    [cp1] rounding in 1a and comments

--- cp1/src/1a.sql
+++ cp1/src/1a.sql
@@ -1,6 +1,10 @@
 SELECT
-    CAST(COUNT(victim.id) AS DECIMAL(10,2)) /
-    CAST((SELECT COUNT(*) FROM data_victim) AS DECIMAL(10,2))
+    ROUND(
+        /* victims of use of force cases */
+        CAST(COUNT(victim.id) AS DECIMAL(10,2)) /
+        /* all victims */
+        CAST((SELECT COUNT(*) FROM data_victim) AS DECIMAL(10,2)),
+    4)
 FROM data_victim victim
 INNER JOIN trr_trr trr
     ON trr.subject_id = victim.id;
