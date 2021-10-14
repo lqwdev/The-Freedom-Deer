973af4f621a02cf372b3884fa0c6faf0e181da15
Qingwei Lan <qingweilan2022@u.northwestern.edu>
2021-10-13 20:35:53 -0500

    [cp1] more sql analytics

--- cp1/src/1e.sql
+++ cp1/src/1e.sql
@@ -4,10 +4,12 @@ SELECT
         CAST(COUNT(*) AS DECIMAL(10,2)) / 
         /* total use of force cases */
         CAST((SELECT COUNT(*) FROM trr_trr) AS DECIMAL(10,2)),
-    6) as percentage
+    6) as percentage,
+    REPLACE(INITCAP(officer.race), 'Asian/Pacific', 'Asian/Pacific Islander')
+        as officer_race,
+    INITCAP(subject_race) as subject_race
 FROM trr_trr trr
 INNER JOIN data_officer officer
     ON trr.officer_id = officer.id
-WHERE 
-    REPLACE(INITCAP(officer.race), 'Asian/Pacific', 'Asian/Pacific Islander') != 
-    INITCAP(subject_race);
+GROUP BY officer_race, subject_race
+ORDER BY percentage DESC;
--- cp1/src/2e.sql
+++ cp1/src/2e.sql
@@ -1,4 +1,13 @@
-SELECT weather_condition, indoor_or_outdoor, lighting_condition, COUNT(*)
+SELECT
+	weather_condition,
+	indoor_or_outdoor,
+	lighting_condition,
+	location_recode,
+	COUNT(*)
 FROM trr_trr
-GROUP BY weather_condition, indoor_or_outdoor, lighting_condition
+GROUP BY
+	weather_condition,
+	indoor_or_outdoor,
+	lighting_condition,
+	location_recode
 ORDER BY COUNT(*) DESC;
