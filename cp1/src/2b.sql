cc2ba114cbcd125c5c54e152154ace5c63100ead
Hualiang Qin <46637518+hualiangqin@users.noreply.github.com>
2021-10-13 21:32:08 -0500

    change count to portion

--- cp1/src/2b.sql
+++ cp1/src/2b.sql
@@ -1,4 +1,7 @@
-SELECT indoor_or_outdoor, COUNT(*)
+SELECT indoor_or_outdoor, ROUND(
+        CAST(COUNT(*) AS DECIMAL(10,2)) /
+        CAST((SELECT COUNT(*) FROM trr_trr) AS DECIMAL(10,2)),
+    6) as percentage
 FROM trr_trr
 GROUP BY indoor_or_outdoor
 ORDER BY COUNT(*) DESC;
--- cp1/src/2d.sql
+++ cp1/src/2d.sql
@@ -1,4 +1,7 @@
-SELECT location_recode, COUNT(*)
+SELECT location_recode, ROUND(
+        CAST(COUNT(*) AS DECIMAL(10,2)) /
+        CAST((SELECT COUNT(*) FROM trr_trr) AS DECIMAL(10,2)),
+    6) as percentage
 FROM trr_trr
 GROUP BY location_recode
 ORDER BY COUNT(*) DESC;
